64c1cc257565038585b91b0590d08cf91ca89244
345andrew <andrew@345.systems>
2017-02-14 16:23:50 +0000

    Boilerplate code from cookiecutter

---
+++ README.md
@@ -0,0 +1,20 @@
+# cloco cli
+
+A command line interface for the cloco API.
+
+
+# Installation
+
+If you don't use `pipsi`, you're missing out.
+Here are [installation instructions](https://github.com/mitsuhiko/pipsi#readme).
+
+Simply run:
+
+    $ pip install cloco-cli
+
+
+# Usage
+
+To use it:
+
+    $ cloco --help
---
+++ setup.py
@@ -0,0 +1,48 @@
+"""
+A command line interface for the cloco API.
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click']
+
+setup(
+    name='cloco-cli',
+    version='0.1.0',
+    url='https://github.com/345systems/cloco-cli',
+    license='BSD',
+    author='345 Systems',
+    author_email='info@345.systems',
+    description='A command line interface for the cloco API.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'cloco = cloco_cli.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
