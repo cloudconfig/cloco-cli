fe6a49479f06540d96438c40efb7c3ed3e54cc3e
345andrew <andrew@345.systems>
2017-02-20 17:08:47 +0000

    version 0.1.3

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ dependencies = ['click', 'requests', 'configparser']
 
 setup(
     name='cloco-cli',
-    version='0.1.2',
+    version='0.1.3',
     license='BSD',
     author='345 Systems',
     author_email='info@345.systems',
     description='A command line interface for the cloco API.',
     url='https://github.com/cloudconfig/cloco-cli',
-    download_url='https://github.com/cloudconfig/cloco-cli/tarball/0.1.2',
+    download_url='https://github.com/cloudconfig/cloco-cli/tarball/0.1.3',
     keywords=['cloco', 'cloudconfig', 'configuration', 'configuration-as-a-service', 'devops'],
     long_description=__doc__,
     packages=find_packages(exclude=['tests']),
